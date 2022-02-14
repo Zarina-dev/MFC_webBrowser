652d86f001f636e2fbda09635af74d0679fa365a
Zarina <61898376+Zarina-dev@users.noreply.github.com>
2022-02-14 12:47:19 +0900

    Add files via upload

---
+++ MFC_webBrowserDlg.cpp
@@ -0,0 +1,183 @@
+﻿
+// MFC_webBrowserDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFC_webBrowser.h"
+#include "MFC_webBrowserDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCwebBrowserDlg 대화 상자
+
+
+
+CMFCwebBrowserDlg::CMFCwebBrowserDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFC_WEBBROWSER_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCwebBrowserDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EXPLORER1, m_web);
+}
+
+BEGIN_MESSAGE_MAP(CMFCwebBrowserDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON_OPENWEB, &CMFCwebBrowserDlg::OnBnClickedButtonOpenweb)
+END_MESSAGE_MAP()
+
+
+// CMFCwebBrowserDlg 메시지 처리기
+
+BOOL CMFCwebBrowserDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	m_web.Navigate(L"https://www.google.com/", NULL, NULL, NULL, NULL);
+
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMFCwebBrowserDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCwebBrowserDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCwebBrowserDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+BEGIN_EVENTSINK_MAP(CMFCwebBrowserDlg, CDialogEx)
+	ON_EVENT(CMFCwebBrowserDlg, IDC_EXPLORER1, 102, CMFCwebBrowserDlg::StatusTextChangeExplorer1, VTS_BSTR)
+END_EVENTSINK_MAP()
+
+
+void CMFCwebBrowserDlg::StatusTextChangeExplorer1(LPCTSTR Text)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+}
+
+
+void CMFCwebBrowserDlg::OnBnClickedButtonOpenweb()
+{
+
+	try {
+		m_web.Navigate(L"http://www.disney.co.kr/home/index.jsp", NULL, NULL, NULL, NULL);
+
+	}
+	catch(int expn)
+	{
+		//SetDlgItemText(IDC_STATIC_info, L"can not navigate to the disney page");
+		AfxMessageBox(L"can not navigate");
+	}
+}
---
+++ MFC_webBrowserDlg.h
@@ -0,0 +1,40 @@
+﻿
+// MFC_webBrowserDlg.h: 헤더 파일
+//
+
+#pragma once
+#include "Explorer1.h"
+
+
+// CMFCwebBrowserDlg 대화 상자
+class CMFCwebBrowserDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCwebBrowserDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_WEBBROWSER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	DECLARE_EVENTSINK_MAP()
+	void StatusTextChangeExplorer1(LPCTSTR Text);
+	CExplorer1 m_web;
+	afx_msg void OnBnClickedButtonOpenweb();
+};
